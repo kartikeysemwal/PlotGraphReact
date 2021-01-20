1b96b2229efa233f04b89c27ac6219dccc80535a
Kartikey Semwal <55513264+kartikeysemwal@users.noreply.github.com>
2021-01-20 20:35:03 +0530

    Added json files in local storage

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,73 +2,15 @@ import React from "react";
 import FusionCharts from "fusioncharts";
 import TimeSeries from "fusioncharts/fusioncharts.timeseries";
 import ReactFC from "react-fusioncharts";
+import data from "../data.json";
+import schema from "../schema.json";
 
 ReactFC.fcRoot(FusionCharts, TimeSeries);
 
 const jsonify = (res) => res.json();
 
-const dataFetch = [
-    ["01:00:00", "Systolic", 120],
-    ["01:00:00", "Diastolic", 60],
-
-    ["01:20:00", "Systolic", 130],
-    ["01:20:00", "Diastolic", 70],
-
-    ["01:40:00", "Systolic", 140],
-    ["01:40:00", "Diastolic", 50],
-
-    ["02:00:00", "Systolic", 110],
-    ["02:00:00", "Diastolic", 40],
-
-    ["02:20:00", "Systolic", 130],
-    ["02:20:00", "Diastolic", 70],
-
-    ["02:40:00", "Systolic", 150],
-    ["02:40:00", "Diastolic", 40],
-
-    ["03:00:00", "Systolic", 170],
-    ["03:00:00", "Diastolic", 80],
-
-    ["03:20:00", "Systolic", 140],
-    ["03:20:00", "Diastolic", 60],
-
-    ["03:40:00", "Systolic", 120],
-    ["03:40:00", "Diastolic", 40],
-
-    ["04:00:00", "Systolic", 170],
-    ["04:00:00", "Diastolic", 50],
-
-    ["04:20:00", "Systolic", 150],
-    ["04:20:00", "Diastolic", 60],
-
-    ["04:40:00", "Systolic", 150],
-    ["04:40:00", "Diastolic", 70],
-
-    ["05:00:00", "Systolic", 170],
-    ["05:00:00", "Diastolic", 80],
-
-    ["05:20:00", "Systolic", 180],
-    ["05:20:00", "Diastolic", 50],
-
-    ["05:40:00", "Systolic", 130],
-    ["05:40:00", "Diastolic", 60],
-];
-
-const schemaFetch = [
-    {
-        name: "Time",
-        type: "date",
-        format: "%H:%M:%S",
-    },
-    {
-        name: "Type",
-        type: "string",
-    },
-    {
-        name: "Blood Pressure",
-        type: "number",
-    },
-];
+// Systolic 80 160
+// Diastolic 40 120
 
 const dataSource = {
     chart: {},
@@ -110,7 +52,7 @@ class ChartViewer extends React.Component {
     }
 
     onFetchData() {
-        Promise.all([dataFetch, schemaFetch]).then((res) => {
+        Promise.all([data, schema]).then((res) => {
             const data = res[0];
             const schema = res[1];
 
